3515c4fcbb2c67a04bba65ea17dd7419c72bf220
sjp261019 <sjp261019@gmail.com>
2022-02-05 23:54:02 -0500

    02/05/22

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    
     //MAKE ARRAY OF 1~13
     var sun = [];
     for(i=1; i<13; i++){
@@ -36,4 +37,21 @@ $(document).ready(function(){
         $(".openMenu").toggle("slow");
         $(".arrow").toggle("slow");
     })
-});
+
+    // ENLARGEN WORK CONTENT / SHOW TEXT ON HOVER
+    $(".work1").children("#workItem1").hover(function (){
+        $(this).toggleClass("workItem2");
+        $("#workItem1 h4").toggle();
+      });
+
+    $(".work2").children().hover(function (){
+        $(this).toggleClass("workItem2");
+        $(this).children("h4").toggle();
+        // $("#workItem3 h4").toggle();
+      });
+
+    // $(".work3").children().hover(function (){
+    //     $(this).toggleClass("workItem2");
+    //   });
+});   
+
